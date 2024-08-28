bc077fd8f0c039e56c0f3173ddd215f19fef6416
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-08-28 17:08:35 +0530

    Dark mode toggle added

--- react-native-assignment/App.tsx
+++ react-native-assignment/App.tsx
@@ -1,11 +1,26 @@
+import {useColorScheme} from 'nativewind';
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {Pressable, SafeAreaView, Text, View} from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
 
 function App(): JSX.Element {
+  const {colorScheme, toggleColorScheme} = useColorScheme();
+
   return (
-    <SafeAreaView style={{}}>
-      <View className="w-full h-full">
-        <Text className="text-black text-xl">Todo</Text>
+    <SafeAreaView>
+      <View className="w-full h-full p-5 dark:bg-[#373737]">
+        <View className="flex flex-row items-center justify-between">
+          <Text className="text-black font-semibold text-[30px] dark:text-white">
+            Todo
+          </Text>
+          <Pressable className="p-3" onPress={toggleColorScheme}>
+            <Feather
+              name={colorScheme === 'light' ? 'moon' : 'sun'}
+              size={30}
+              color={colorScheme === 'light' ? 'black' : 'white'}
+            />
+          </Pressable>
+        </View>
       </View>
     </SafeAreaView>
   );
